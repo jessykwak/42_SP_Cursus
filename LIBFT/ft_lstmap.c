152a8a6a8fb8ecd6cc442efcb6ff6a405750bab7
Jessica <jessica92kr@gmail.com>
2023-09-19 00:48:31 -0300

    lstmap

--- LIBFT/ft_lstmap.c
+++ LIBFT/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmin-kwa <jmin-kwa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jessk <jessk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 21:28:12 by jmin-kwa          #+#    #+#             */
-/*   Updated: 2023/09/11 21:48:28 by jmin-kwa         ###   ########.fr       */
+/*   Updated: 2023/09/19 00:47:28 by jessk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 
+	if (!lst)
+		return (NULL);
+	while (lst)
+	{
+		ft_lstadd_back(&new_lst, ft_lstnew(f(lst->content)));
+		if (!new_lst)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		lst = lst->next;
+	}
 	return (new_lst);
 }
